c9d8503490bc538efa80d58ba5ff3e07dafe0b5f
Marcos Vázquez <41173218+MARCOSBLACKER7@users.noreply.github.com>
2019-01-05 17:28:25 -0600

    Add files via upload

---
+++ 3th/ANÁLISIS VECTORIAL/PROYECTO/PruebaConvex2.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double ERROR = 1e-9;
+
+// Tolerancia en flotantes.
+bool Igual(double a, double b) { 
+    return fabs(a - b) < ERROR;
+}
+
+
+// Punto en 2D.
+struct Punto {
+    double x, y;
+
+    Punto() : x(), y() {}
+    Punto(double X, double Y) : x(X), y(Y) {}
+
+    // Izquierda a derecha, abajo a arriba.
+    bool operator<(const Punto& cmp) const {
+        if (!Igual(x, cmp.x)) return x < cmp.x;
+        return Igual(y, cmp.y)? false: y < cmp.y;
+    }
+ 
+    bool operator==(const Punto& cmp) const {
+        return Igual(x, cmp.x) && Igual(y, cmp.y);
+    } 
+};
+    
+typedef vector<Punto> Poligono;
+
+// Producto cruz entre vectores v y w.
+double Cruz(const Punto& v, const Punto& w) {
+    return v.x * w.y - v.y * w.x;
+}
+
+// Trasladar p tomando como origen al punto o .
+Punto Trasladar(const Punto& o, const Punto& p) {
+    return Punto(p.x - o.x, p.y - o.y);
+}
+
+//Producto cruz entre dos vectores
+int ManoDerecha(const Punto& o, const Punto& p, const Punto& q) {
+    double ccw = Cruz(Trasladar(o, p), Trasladar(o, q));
+    return Igual(ccw, 0)? 0: (ccw < 0)? -1: 1;
+}
+	
+	// Cerco convexo de un conjunto de puntos.
+Poligono CercoConvexo(vector<Punto> P)
+{
+    // Si ya esta ordenado, no usar sort.
+    sort(P.begin(), P.end());
+    Poligono arriba, abajo;
+   		 for (int i = 0; i < P.size(); ++i) {
+       		 while (arriba.size() > 1)
+			{
+            	int p = arriba.size() - 1;
+            	// Permitir colineales: <=
+            	if (ManoDerecha(arriba[p - 1],
+                	arriba[p], P[i]) < 0) break;
+            	arriba.pop_back();
+        	}
+        arriba.push_back(P[i]);
+    }
+    arriba.pop_back();
+    for (int i = P.size() - 1; i >= 0; --i) {
+        while (abajo.size() > 1) {
+            int p = abajo.size() - 1;
+            // Permitir colineales: <=
+            if (ManoDerecha(abajo[p - 1],
+                abajo[p], P[i]) < 0) break;
+            abajo.pop_back();
+        }
+        abajo.push_back(P[i]);
+    }
+    arriba.insert(arriba.end(),
+        abajo.begin(), abajo.end());
+    return arriba; // Convex hull.
+}
+
+
+FuncionPrincipal()
+{
+		int N ;
+	char Y,K;
+	ofstream salida("grafico.py"); 
+	srand(time(NULL));
+	
+	printf("Cuantos puntos deseas colocar aleatoriamente?\n");
+	cin >> N ;
+	fflush(stdin);
+	
+	vector<Punto> pts(N); 
+	
+	for(int i = 0; i < N ; i++){
+		int x, y ;
+		x = rand()%492;
+		y = rand()%492;
+		pts[i].x = x+10;
+		pts[i].y = y+10;
+	
+	
+		}
+	salida << "from graphics import *\n";
+	salida << "def main():\n";
+	salida << "\twin = GraphWin(\"Convexo\", 512, 512)\n";
+	
+	for(int i = 0 ; i < N ; i++){
+		salida << "\tc" << i << " = Circle( Point(" << pts[i].x << ',' << pts[i].y << ") , 3 )\n";
+		salida << "\tc" << i << ".setFill(\"black\")\n";
+		salida << "\tc" << i << ".draw(win)\n";
+	}
+	salida <<'\n';
+	
+	Poligono P;
+	P = CercoConvexo(pts);
+	
+	for(int i = 0 ; i < P.size()-1 ; i++){
+		salida << "\tl" << i << " = Line(Point("<<P[i].x << ","<< P[i].y << "),Point("<< P[i+1].x << ","<< P[i+1].y << "))\n";
+		salida << "\tl" << i << ".setOutline(\"blue\")\n";
+		salida << "\tl" << i << ".setWidth(3)\n";
+		salida << "\tl" << i << ".draw(win)\n";
+	}
+	salida << "\tl" << P.size()-1 << " = Line(Point("<< P.back().x << ","<< P.back().y << "),Point("<< P[0].x << ","<< P[0].y << "))\n";
+	salida << "\tl" << P.size()-1 << ".setOutline(\"blue\")\n";
+	salida << "\tl" << P.size()-1 << ".setWidth(3)\n";
+	salida << "\tl" << P.size()-1 << ".draw(win)\n";
+		
+	salida << "\twin.getMouse() \n";
+	salida << "\twin.close()\n";
+	salida << "main()\n";
+	
+	salida.close();
+	
+	system("python grafico.py");
+//	system("pause");
+	
+}
+
+int main (){
+	char K;
+	FuncionPrincipal();
+	printf("Desea repetir el proceso? (1=Y/N)\n");
+		
+			cin >> K;
+			if(K == 'y' || K == 'Y'){
+				FuncionPrincipal();
+				
+			}
+
+	}
+
+
---
+++ 3th/ESTRUCTURAS DE DATOS/PRÁCTICA 2/cajeras.txt
@@ -0,0 +1,106 @@
+/*
+AUTOR: Edgardo Adrián Franco Martínez (C) Septiembre 2012
+VERSIÓN: 1.3
+
+DESCRIPCIÓN: Ejemplo de atención de dos cola de clientes, la simulación
+de tiempo se reliza con la función Sleep(Solo funciona en Windows), cada X
+tiempo llega un cliente y escoje pseudoaleatoriamente una cola y el tiempo 
+de atención es Y.
+
+OBSERVACIONES: Se puede emplear la libreria TADColaEst.h, TADPilaEstCir.h o TADColaDin.h implementadas
+en clase. Ambas estructuras elemento, ya sea la de las implementaciónes estáticas o dinámica deberán 
+tener un campo int n;
+
+COMPILACIÓN: 	gcc -o Cajeras Cajeras.c TADCola/TADCola(Din|Est|EstCirc).o (Si se tiene el objeto de la implementación)
+				gcc -o Cajeras Cajeras.c TADCola/TADCola(Din|Est|EstCirc).c (Si se tiene el fuente de la implementación)
+
+EJECUCIÓN: Cajeras.exe (En Windows)
+*/
+
+//LIBRERAS
+#include <stdio.h>
+#include <windows.h>				//Funciona unicamente en Windows para usar la función Sleep()
+//#include "TADCola/TADColaEst.h" 	//Si se usa la implemtentación dinámica (TADColaDin.c)
+#include "TADCola/TADColaDin.h" 	//Si se usa la implemtentación estática (TADColaEst.c|TADColaEstCirc.c)
+
+//DEFINICION DE CONSTANTES
+#define TIEMPO_BASE	200			//Tiempo base en ms
+#define TIEMPO_CLIENTE	1		//Tiempo base en ms * 1
+#define TIEMPO_ATENCION	3		//Tiempo base en ms * 3
+
+int main(void)
+{
+	unsigned int tiempo = 0;
+	unsigned int cliente = 0;
+	int i,fila;
+	elemento e;
+	//Inicializar la función rand
+	srand(time(NULL));
+	
+	//Crear dos colas
+	cola cajera[2];
+	
+	//Inicializar ambas colas
+	Initialize(&cajera[0]);
+	Initialize(&cajera[1]);
+	
+	//Ciclo infinito
+	while (1)
+	{
+		Sleep(TIEMPO_BASE);		//Esperar el tiempo base
+		tiempo++;				//Incrementar el contador de tiempo
+		
+		//Si el tiempo es multiplo del tiempo de atencion
+		if (tiempo % TIEMPO_ATENCION == 0)
+		{
+			//Cajera 0
+			if (!Empty(&cajera[0]))
+			{
+				e = Dequeue(&cajera[0]);
+				printf("\n\n\nAtendi a: %d en caja 0", e.n);
+			}
+			else
+			{
+				printf("\n\n\nNo hay alguien por atender en caja 0");
+			}
+			//Cajera 1
+			if (!Empty(&cajera[1]))
+			{
+				e = Dequeue(&cajera[1]);
+				printf("\n\n\nAtendi a: %d en caja 1", e.n);
+			}
+			else
+			{
+				printf("\n\n\nNo hay alguien por atender en caja 1");
+			}
+		}
+		//Si el tiempo es multiplo del de llegada de los clientes
+		if (tiempo % TIEMPO_CLIENTE == 0)
+		{
+			cliente++;				//Incrementar el numero de clientes
+			e.n = cliente;			//Dar el numero que identifica al cliente
+			fila=rand()%2;			//Escoger la fila para formarse aleatoriamente					
+			Queue(&cajera[fila], e);//Formarse en la fila seleccionada
+			printf("\n\n\nLlego el cliente: %d a la cola de la caja %d", e.n,fila);
+		}
+		
+		//Mostrar los clientes en cada cola
+		printf("\n\n%d clientes en cola 0: [",Size(&cajera[0]));
+		for (i=1;i<=Size(&cajera[0]);i++)
+		{
+			e=Element(&cajera[0],i);
+			printf("%d\t", e.n);
+		}
+		printf("]");
+		
+		
+		printf("\n\n%d clientes en cola 1: [",Size(&cajera[1]));
+		for (i=1;i<=Size(&cajera[1]);i++)
+		{
+			e=Element(&cajera[1],i);
+			printf("%d\t", e.n);
+		}
+		printf("]");
+	}
+	return 0;
+}
